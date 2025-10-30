b18a7da08a8048f32a832bdfe3f86da1766ab07d
Jacob <jacovaut@gmail.com>
2025-10-30 17:22:13 -0300

    micro_ros

--- pio/src/main.cpp
+++ pio/src/main.cpp
@@ -1,18 +1,36 @@
-#include <Arduino.h>
+#include <micro_ros_arduino.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/int32.h>
 
-// put function declarations here:
-int myFunction(int, int);
+rcl_publisher_t publisher;
+rcl_node_t node;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_init_options_t init_options;
+std_msgs__msg__Int32 msg;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // Initialize micro-ROS transport over serial
+  set_microros_transports();
+
+  allocator = rcl_get_default_allocator();
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  rclc_node_init_default(&node, "esp32_node", "", &support);
+  rclc_publisher_init_default(
+      &publisher,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+      "esp32_counter"
+  );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  static int count = 0;
+  msg.data = count++;
+  rcl_publish(&publisher, &msg, NULL);
+  delay(1000);
 }
