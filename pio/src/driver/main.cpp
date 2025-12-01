2b22414a071f9bcd66d9ab59a2b8321e70c4ed9b
Jacovaut <jacovaut@gmail.com>
2025-12-01 12:26:16 -0400

    fixed driver code for negative speeds

--- pio/src/driver/main.cpp
+++ pio/src/driver/main.cpp
@@ -29,8 +29,10 @@
 //
 // jacov 2025
 
+#include "MicroRos.h"
 #include <Arduino.h>
 #include "MotorDriver.h"
+
 
 FastAccelStepperEngine engine;
 
@@ -38,21 +40,23 @@ FastAccelStepperEngine engine;
 MotorDriver motor1(17, 16, 4);
 MotorDriver motor2(15, 13, 12);
 MotorDriver motor3(26, 25, 33);
-MotorDriver motor4(23,22,21);
+MotorDriver motor4(23, 22, 21);
+MotorDriver* motors[] = { &motor1, &motor2, &motor3, &motor4 };
 
 // Robot motion and dimmention variables
 float vx = 0; // m/s
 float vy = 0; // m/s
 float w  = 0; // rad/s
 
-const float lxy = 0.12 + 0.12; // lx (m) + ly (m) = 240mm
+const float lxy = 0.125 + 0.2; // lx (m) + ly (m) = 240mm
 
-const float r = 0.05; //m   (100mm mecanum)
+const float r = 0.03; //m   (100mm mecanum)
 
 void calculateWheelSpeeds(float vx, float vy, float w, float* wheelSpeeds);
-void setSpeeds(float new_vx, float new_vy, float new_w);
+void setSpeed(float new_vx, float new_vy, float new_w);
 
 void setup() {
+
     pinMode(2, OUTPUT);
     pinMode(27, OUTPUT);
     pinMode(32, OUTPUT);
@@ -69,50 +73,39 @@ void setup() {
     engine.init();
     
     motor1.begin(engine);
-    // motor2.begin(engine);
+    motor2.begin(engine);
     motor3.begin(engine);
-    // motor4.begin(engine);
+    motor4.begin(engine);
 
-    motor1.setSpeedRPM(60);
-    // motor2.setSpeedRPM(60);
-    motor3.setSpeedRPM(60);
-    // motor4.setSpeedRPM(60);
-
-    motor1.runForward();
-    // motor2.runForward();
-    motor3.runForward();
-    // motor4.runForward();
-
-    // motor1.step();
-    // motor2.step();
-    // motor3.step();
-    // motor4.step();
-
-    motor1.clearStatus();
-    // motor2.clearStatus();
-    motor3.clearStatus();
-    // motor4.clearStatus();
-
-    delay(3000);
-
-    motor1.print_status();
+    Serial.println("Motors initialized.");
 }
 
 void loop()
 {
+    bool minus = false;
     // Example: set different speeds using serial input
-    // if(Serial.available()) {
-    //     String input = Serial.readString();
-        
-    //     Serial.print("Vx = ");
-    //     Serial.println(input.toFloat());
+    if(Serial.available()) {
+        String input = Serial.readString();
 
-    //     setSpeeds(input.toFloat(), 0, 0);
-    // }
+        if (input.startsWith("-")) {
+            minus = true;
+        }
+        
+        Serial.print("Vx = ");
+        Serial.println(input.toFloat());
+
+        if (minus) {
+            setSpeed(input.toFloat(), 0, 0);
+            minus = false;
+        }
+        else {
+        setSpeed(input.toFloat(), 0, 0);
+        }
+    }
 }
 
 // set robot speeds in m/s and wheel angular velocity in rad/s
-void setSpeeds(float new_vx, float new_vy, float new_w) {
+void setSpeed(float new_vx, float new_vy, float new_w) {
     vx = new_vx;
     vy = new_vy;
     w  = new_w;
@@ -131,17 +124,24 @@ void setSpeeds(float new_vx, float new_vy, float new_w) {
     // get the wheel speeds in rad/s
     calculateWheelSpeeds(vx, vy, w, wheelSpeeds);
 
-    // set motor speeds in RPM ( rad/s * 60 / (2 * PI) = RPM )
-    motor1.setSpeedRPM(wheelSpeeds[0] * 60 / (2 * PI));
-    motor2.setSpeedRPM(wheelSpeeds[1] * 60 / (2 * PI));
-    motor3.setSpeedRPM(wheelSpeeds[2] * 60 / (2 * PI));
-    motor4.setSpeedRPM(wheelSpeeds[3] * 60 / (2 * PI));
+    for (int i = 0; i < 4; i++) {
+        float speed = abs(wheelSpeeds[i]);
+        Serial.printf("Wheel %d speed (rad/s): %.2f\n", i, speed);
 
-    // run motors
-    motor1.runForward();
-    motor2.runForward();
-    motor3.runForward();
-    motor4.runForward();
+        // limit wheel speeds to max 10 rad/s
+        if (speed > 10.0) {
+            speed = 10.0;
+        }
+
+        motors[i]->setSpeedRPM(speed * 60 / (2 * PI)); // set speed in RPM
+
+        if (wheelSpeeds[i] < 0) {
+            motors[i]->runBackward();
+        }
+        else {
+            motors[i]->runForward();
+        }
+    }
 
     // debug print
     Serial.printf("Wheel Speeds (rad/s): FL: %.2f, FR: %.2f, RL: %.2f, RR: %.2f\n", 
