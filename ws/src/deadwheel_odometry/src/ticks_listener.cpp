776342ce2f6e820e3ebba8e40d39fafc15939644
Jacob Gaudet <jacobgaudet13@gmail.com>
2026-02-03 21:41:12 -0400

    update deadwheel odo

--- ws/src/deadwheel_odometry/src/ticks_listener.cpp
+++ ws/src/deadwheel_odometry/src/ticks_listener.cpp
@@ -2,7 +2,11 @@
 #include "deadwheel_msgs/msg/deadwheel_ticks.hpp"
 #include <cmath>
 
-double x_{0.0}, y_{0.0}, theta_{0.0};
+double g_x{0.0}, g_y{0.0}, g_theta{0.0};
+
+double normalizeAngleSigned(double angle) {
+    return std::remainder(angle, 2.0 * M_PI);
+}
 
 class TicksListener : public rclcpp::Node
 {
@@ -16,7 +20,7 @@ class TicksListener : public rclcpp::Node
     const double DEADWHEEL_DISTANCE = 10; //distance entre les deux deadwheel principaux
     const double OFFSET = 2; //distance entre le side deadwheel et le centre de rotation du robot
 
-    double prevTicks[3] = {0, 0, 0};
+    int64_t prevTicks[3] = {0, 0, 0};
     bool initialized_{false};
     rclcpp::Time prev_stamp_;
 
@@ -29,9 +33,9 @@ class TicksListener : public rclcpp::Node
         const rclcpp::Time stamp(msg->header.stamp);
 
         if (!initialized_) {
-          prevTicks[0] = 0;
-          prevTicks[1] = 0;
-          prevTicks[2] = 0;
+          prevTicks[0] = msg->t0;
+          prevTicks[1] = msg->t1;
+          prevTicks[2] = msg->t2;
           prev_stamp_ = stamp;
           initialized_ = true;
          return;
@@ -46,34 +50,34 @@ class TicksListener : public rclcpp::Node
         }
 
         // calculs des delta ticks
-        double ticks0 = (static_cast<double>(msg->t0)); 
-        double ticks1 = (static_cast<double>(msg->t1));
-        double ticks2 = (static_cast<double>(msg->t2));
-        double dRTicks = ticks0 - prevTicks[0];
-        double dLTicks = ticks1 - prevTicks[1];
-        double dSTicks = ticks2 - prevTicks[2];
+        int64_t ticks0 = (msg->t0); 
+        int64_t ticks1 = (msg->t1);
+        int64_t ticks2 = (msg->t2);
+        int64_t dRTicks = ticks0 - prevTicks[0];
+        int64_t dLTicks = ticks1 - prevTicks[1];
+        int64_t dSTicks = ticks2 - prevTicks[2];
         prevTicks[0] = ticks0;
         prevTicks[1] = ticks1;
         prevTicks[2] = ticks2;
 
         //calculs des déplacements selon les axes du robot
-        double rightDist = dRTicks * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[0];
-        double leftDist = dLTicks * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[1];
-        double dyr = 0.5 * (rightDist + leftDist);
+        double rightDist = static_cast<double>(dRTicks) * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[0];
+        double leftDist = static_cast<double>(dLTicks) * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[1];
+        double dxr = 0.5 * (rightDist + leftDist);
         double dangle = (rightDist - leftDist) / DEADWHEEL_DISTANCE;
-        double dxr = (dSTicks * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[2]) - OFFSET * dangle;
-        double avgangle = deadwheelodo.angle + dangle/2; 
+        double dyr = (static_cast<double>(dSTicks) * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[2]) - OFFSET * dangle;
+        double avgangle = g_theta + dangle/2; 
 
         //calculs des déplacements selon le world
-        double cos = std::cos(avgangle);
-        double sin = std::sin(avgangle);
-        double dx = dyr*cos - dxr*sin;
-        double dy = dyr*sin + dxr*cos;
+        double c = std::cos(avgangle);
+        double s = std::sin(avgangle);
+        double dx = dxr*c - dyr*s;
+        double dy = dyr*c + dxr*s;
 
         //mise à jour de la position du robot
-        x_ += dx;
-        y_ += dy;
-        theta_ = AngleUtils.normalizeRadians(theta_ + dangle);
+        g_x += dx;
+        g_y += dy;
+        g_theta = normalizeAngleSigned(g_theta + dangle);
 
         //calculs de vitesse
         double vx = dx/dt;
@@ -83,14 +87,12 @@ class TicksListener : public rclcpp::Node
         RCLCPP_INFO(
         this->get_logger(),
         "x=%.3f y=%.3f th=%.3f | vx=%.3f vy=%.3f w=%.3f",
-        x_, y_, theta_, vx, vy, omega
+        g_x, g_y, g_theta, vx, vy, omega
        );
       }
     ); 
-  };
-}
-
-
+  }
+};
 
 int main(int argc, char * argv[])
 {
