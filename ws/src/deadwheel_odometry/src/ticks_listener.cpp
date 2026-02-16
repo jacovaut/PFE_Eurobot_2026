6b5af51c6edbe9c2da551d439db0085ea4e431f6
Jacob Gaudet <jacobgaudet13@gmail.com>
2026-02-16 16:51:30 -0400

    added tf transform and odom

--- ws/src/deadwheel_odometry/src/ticks_listener.cpp
+++ ws/src/deadwheel_odometry/src/ticks_listener.cpp
@@ -1,8 +1,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include "deadwheel_msgs/msg/deadwheel_ticks.hpp"
 #include <cmath>
-
-double g_x{0.0}, g_y{0.0}, g_theta{0.0};
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <mutex>
 
 double normalizeAngleSigned(double angle) {
     return std::remainder(angle, 2.0 * M_PI);
@@ -12,6 +15,14 @@ class TicksListener : public rclcpp::Node
 {
   private : 
     rclcpp::Subscription<deadwheel_msgs::msg::DeadwheelTicks>::SharedPtr subscription_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    double x__{0.0}, y__{0.0}, theta__{0.0};
+    double vx{0}, vy{0}, omega{0};
+    std::mutex mtx_;
   
     //Constantes, à ajouter les bonnes valeurs
     const double ENCODER_TICKS_PER_REVOLUTION [3] = {1000, 1000, 1000};
@@ -24,12 +35,80 @@ class TicksListener : public rclcpp::Node
     bool initialized_{false};
     rclcpp::Time prev_stamp_;
 
+    void publishOdomAndTf()
+    {
+      std::lock_guard<std::mutex> lock(mtx_);
+      
+      if (!initialized_)
+        return;
+
+      rclcpp::Time stamp = this->now();
+
+      // Create quaternion from yaw
+      tf2::Quaternion q;
+      q.setRPY(0.0, 0.0, theta__);
+      q.normalize();
+
+      // Create Odometry message
+      nav_msgs::msg::Odometry odom;
+      odom.header.stamp = stamp;
+      odom.header.frame_id = "odom";
+      odom.child_frame_id = "base_link";
+
+      // Pose
+      odom.pose.pose.position.x = x__;
+      odom.pose.pose.position.y = y__;
+      odom.pose.pose.position.z = 0.0;
+      odom.pose.pose.orientation.x = q.x();
+      odom.pose.pose.orientation.y = q.y();
+      odom.pose.pose.orientation.z = q.z();
+      odom.pose.pose.orientation.w = q.w();
+      odom.pose.covariance[0]  = 1e-3;  // x
+      odom.pose.covariance[7]  = 1e-3;  // y
+      odom.pose.covariance[35] = 5e-2;  // yaw
+
+      // Twist (robot frame)
+      odom.twist.twist.linear.x = vx;
+      odom.twist.twist.linear.y = vy;
+      odom.twist.twist.angular.z = omega;
+      odom.twist.covariance[0]  = 1e-2;  // vx
+      odom.twist.covariance[7]  = 1e-2;  // vy
+      odom.twist.covariance[35] = 1e-1;  // omega
+
+      odom_pub_->publish(odom);
+
+      // TF transform
+      geometry_msgs::msg::TransformStamped tf;
+      tf.header.stamp = stamp;
+      tf.header.frame_id = "odom";
+      tf.child_frame_id = "base_link";
+
+      tf.transform.translation.x = x__;
+      tf.transform.translation.y = y__;
+      tf.transform.translation.z = 0.0;
+      tf.transform.rotation.x = q.x();
+      tf.transform.rotation.y = q.y();
+      tf.transform.rotation.z = q.z();
+      tf.transform.rotation.w = q.w();
+
+      tf_broadcaster_->sendTransform(tf);
+    }
+
   public:
     TicksListener() : Node("ticks_listener")
     {
+      odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("odom_deadwheels", 10);
+      tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+      timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(20),
+      std::bind(&TicksListener::publishOdomAndTf, this));
+
       subscription_ = this->create_subscription<deadwheel_msgs::msg::DeadwheelTicks>(
-      "deadwheel_ticks", 10,[this](deadwheel_msgs::msg::DeadwheelTicks::SharedPtr msg)
+      "deadwheel_ticks", 50,[this](deadwheel_msgs::msg::DeadwheelTicks::SharedPtr msg)
       {
+        std::lock_guard<std::mutex> lock(mtx_);
+
         const rclcpp::Time stamp(msg->header.stamp);
 
         if (!initialized_) {
@@ -66,7 +145,7 @@ class TicksListener : public rclcpp::Node
         double dxr = 0.5 * (rightDist + leftDist);
         double dangle = (rightDist - leftDist) / DEADWHEEL_DISTANCE;
         double dyr = (static_cast<double>(dSTicks) * DEADWHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION[2]) - OFFSET * dangle;
-        double avgangle = g_theta + dangle/2; 
+        double avgangle = theta__ + dangle/2; 
 
         //calculs des déplacements selon le world
         double c = std::cos(avgangle);
@@ -75,20 +154,21 @@ class TicksListener : public rclcpp::Node
         double dy = dyr*c + dxr*s;
 
         //mise à jour de la position du robot
-        g_x += dx;
-        g_y += dy;
-        g_theta = normalizeAngleSigned(g_theta + dangle);
+        x__ += dx;
+        y__ += dy;
+        theta__ = normalizeAngleSigned(theta__ + dangle);
 
         //calculs de vitesse
-        double vx = dx/dt;
-        double vy = dy/dt;
-        double omega = dangle/dt;
+        vx = dxr/dt;
+        vy = dyr/dt;
+        omega = dangle/dt;
 
-        RCLCPP_INFO(
+        RCLCPP_INFO_THROTTLE(
         this->get_logger(),
-        "x=%.3f y=%.3f th=%.3f | vx=%.3f vy=%.3f w=%.3f",
-        g_x, g_y, g_theta, vx, vy, omega
-       );
+        *this->get_clock(), 500,  // milliseconds
+        "x=%.3f y=%.3f", 
+        x__, y__
+        );
       }
     ); 
   }
