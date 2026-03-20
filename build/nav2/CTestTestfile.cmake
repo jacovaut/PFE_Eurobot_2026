6c40941477387b29ed538a4ea869b7c1de87dff0
IsaRic00770 <IsaRic00770@gmail.com>
2026-03-20 16:00:52 -0300

    removed build, install and log from PFE_Eurobot2026

--- build/nav2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/com2001/PFE_Eurobot_2026/ws/src/nav2
-# Build directory: /home/com2001/PFE_Eurobot_2026/build/nav2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
