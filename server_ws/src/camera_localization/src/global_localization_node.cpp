458333a31e0d581f5e208cefed8a52b8936a51c4
Jacob Gaudet <jacobgaudet13@gmail.com>
2026-04-14 15:13:14 -0300

    computer sharing 1

--- server_ws/src/camera_localization/src/global_localization_node.cpp
+++ server_ws/src/camera_localization/src/global_localization_node.cpp
@@ -203,6 +203,20 @@ private:
     cap_.set(cv::CAP_PROP_BUFFERSIZE, 1);
 
     RCLCPP_INFO(get_logger(), "Opened camera %s", device_.c_str());
+
+    RCLCPP_INFO(get_logger(), "Actual width: %.0f", cap_.get(cv::CAP_PROP_FRAME_WIDTH));
+    RCLCPP_INFO(get_logger(), "Actual height: %.0f", cap_.get(cv::CAP_PROP_FRAME_HEIGHT));
+    RCLCPP_INFO(get_logger(), "Actual fps: %.2f", cap_.get(cv::CAP_PROP_FPS));
+
+    int fourcc = static_cast<int>(cap_.get(cv::CAP_PROP_FOURCC));
+    char fcc[] = {
+      static_cast<char>(fourcc & 0xFF),
+      static_cast<char>((fourcc >> 8) & 0xFF),
+      static_cast<char>((fourcc >> 16) & 0xFF),
+      static_cast<char>((fourcc >> 24) & 0xFF),
+      '\0'
+    };
+    RCLCPP_INFO(get_logger(), "Actual FOURCC: %s", fcc);
   }
 
   void loadCalibration()
@@ -1046,36 +1060,3 @@ int main(int argc, char **argv)
   return 0;
 }
 
-#!/usr/bin/env bash
-# Device
-DEV=/dev/eurobot2026-ELPcamera
- 
-# 1) Kill flicker in Canada (60 Hz)
-v4l2-ctl -d $DEV --set-ctrl=power_line_frequency=2
- 
-# 2) Exposure mode + exposure time (manual)
-# auto_exposure: 1 = Manual Mode (as shown by your driver)
-v4l2-ctl -d $DEV --set-ctrl=auto_exposure=1
-v4l2-ctl -d $DEV --set-ctrl=exposure_time_absolute=200
- 
-# 3) Keep gain low (cleaner edges)
-v4l2-ctl -d $DEV --set-ctrl=gain=40
- 
-# 4) White balance: keep AUTO ON (temperature becomes inactive)
-v4l2-ctl -d $DEV --set-ctrl=white_balance_automatic=1
- 
-# 5) Basic image tuning (your current values)
-v4l2-ctl -d $DEV --set-ctrl=brightness=0
-v4l2-ctl -d $DEV --set-ctrl=contrast=0
-v4l2-ctl -d $DEV --set-ctrl=saturation=70
-v4l2-ctl -d $DEV --set-ctrl=hue=0
-v4l2-ctl -d $DEV --set-ctrl=gamma=120
-v4l2-ctl -d $DEV --set-ctrl=sharpness=1
-v4l2-ctl -d $DEV --set-ctrl=backlight_compensation=0
- 
-# 6) Focus: manual lock at your current value
-v4l2-ctl -d $DEV --set-ctrl=focus_automatic_continuous=0
-v4l2-ctl -d $DEV --set-ctrl=focus_absolute=1023
- 
-# (Optional) Check what you ended up with
-v4l2-ctl -d $DEV --all | sed -n '/User Controls/,$p'
