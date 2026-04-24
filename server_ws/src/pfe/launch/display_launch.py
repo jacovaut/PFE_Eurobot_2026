c66cede5bda016b734dddbbb187209e47e9ac74b
IsaacRic <IsaRic00770@gmail.com>
2026-04-24 10:43:57 -0300

    changes agane

--- server_ws/src/pfe/launch/display_launch.py
+++ server_ws/src/pfe/launch/display_launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
         jsp_exec = 'joint_state_publisher'
 
     robot_description = ParameterValue(
-        Command(['ros2 run xacro xacro ', urdf_file]),
+        Command(['xacro ', urdf_file]),
         value_type=str
     )
 
