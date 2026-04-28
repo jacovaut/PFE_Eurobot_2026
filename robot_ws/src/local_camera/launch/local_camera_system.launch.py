db2741341d149d95d3b8dd393e00d88b952917ba
IsaacRic <IsaRic00770@gmail.com>
2026-04-28 14:19:02 -0300

    changed file locations to robot_ws

---
+++ robot_ws/src/local_camera/launch/local_camera_system.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='local_camera',
+            executable='local_perception_node',
+            name='local_perception_node',
+            output='screen',
+            parameters=[{
+                'camera_mode': 'stream',
+                'stream_url': 'tcp://127.0.0.1:8888',
+                'camera_device': 0,
+                'show_debug_window': False,
+                'debug_save_image': True,
+            }],
+        ),
+        Node(
+            package='local_camera',
+            executable='local_pickup_solver_node',
+            name='local_pickup_solver_node',
+            output='screen',
+        ),
+    ])
