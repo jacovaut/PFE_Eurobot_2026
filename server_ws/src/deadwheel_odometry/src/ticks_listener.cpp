7d3ed87e1f6cc8cffbaec7c7be2383147c33fb1b
IsaRic00770 <IsaRic00770@gmail.com>
2026-05-01 13:00:06 -0300

    joy + ticks_listner tune

--- server_ws/src/deadwheel_odometry/src/ticks_listener.cpp
+++ server_ws/src/deadwheel_odometry/src/ticks_listener.cpp
@@ -28,7 +28,7 @@ class TicksListener : public rclcpp::Node
     const double SDEADWHEEL_DIAMETER  = 0.0384;
     const double DEADWHEEL_CIRCUMFERENCE = (M_PI) * DEADWHEEL_DIAMETER;
     const double SDEADWHEEL_CIRCUMFERENCE = (M_PI) * SDEADWHEEL_DIAMETER;
-    const double DEADWHEEL_DISTANCE = 0.14994; //distance entre les deux deadwheel principaux, anciennement 0.1446
+    const double DEADWHEEL_DISTANCE = 0.1496; //distance entre les deux deadwheel principaux, anciennement 0.1446
     const double OFFSET = -0.063; //distance entre le side deadwheel et le centre de rotation du robot
 
     int64_t prevTicks[3] = {0, 0, 0};
@@ -93,9 +93,9 @@ class TicksListener : public rclcpp::Node
   public:
     TicksListener() : Node("ticks_listener")
     {
-      const double initial_x = this->declare_parameter<double>("initial_x", 0.0);
-      const double initial_y = this->declare_parameter<double>("initial_y", 0.0);
-      const double initial_yaw_deg = this->declare_parameter<double>("initial_yaw_deg", 0.0);
+      const double initial_x = this->declare_parameter<double>("initial_x", 0.265);
+      const double initial_y = this->declare_parameter<double>("initial_y", 1.78);
+      const double initial_yaw_deg = this->declare_parameter<double>("initial_yaw_deg", 90.0);
 
       x__ = initial_x;
       y__ = initial_y;
