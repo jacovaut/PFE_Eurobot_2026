6d1138bd16b29c9309abd902b734e2f23d3862ab
Jacob Gaudet <jacobgaudet13@gmail.com>
2026-05-06 19:29:45 -0300

    no ws

--- ws/build/micro_ros_agent/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/com2001/PFE_Eurobot_2026/ws/src/uros/micro-ROS-Agent/micro_ros_agent
-# Build directory: /home/com2001/PFE_Eurobot_2026/ws/build/micro_ros_agent
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/com2001/PFE_Eurobot_2026/ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
